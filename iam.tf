4f7d15c28bda3267583027ad3b31402cfbb55b95
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-04-22 09:39:28 -0700

    feat: `cognito_es_policy` use predefined `AmazonOpenSearchServiceCognitoAccess`

--- iam.tf
+++ iam.tf
@@ -1,25 +1,6 @@
-data "aws_iam_policy_document" "cognito_es_policy" {
-  count   = var.cognito_enabled ? 1 : 0
-  version = "2012-10-17"
-  statement {
-    effect = "Allow"
-    actions = [
-      "cognito-idp:DescribeUserPool",
-      "cognito-idp:CreateUserPoolClient",
-      "cognito-idp:DeleteUserPoolClient",
-      "cognito-idp:DescribeUserPoolClient",
-      "cognito-idp:AdminInitiateAuth",
-      "cognito-idp:AdminUserGlobalSignOut",
-      "cognito-idp:ListUserPoolClients",
-      "cognito-identity:DescribeIdentityPool",
-      "cognito-identity:UpdateIdentityPool",
-      "cognito-identity:SetIdentityPoolRoles",
-      "cognito-identity:GetIdentityPoolRoles"
-    ]
-    resources = [
-      "*",
-    ]
-  }
+data "aws_iam_policy" "cognito_es_policy" {
+  count = var.cognito_enabled ? 1 : 0
+  name  = "AmazonOpenSearchServiceCognitoAccess"
 }
 
 data "aws_iam_policy_document" "es_assume_policy" {
@@ -37,17 +18,15 @@ data "aws_iam_policy_document" "es_assume_policy" {
 
 resource "aws_iam_policy" "cognito_es_policy" {
   count  = var.cognito_enabled ? 1 : 0
-  name   = "${var.name}-COGNITO-ACCESS-ES-POLICY"
+  name   = "${var.name}_CognitoAccessForAmazonOpenSearch"
   policy = data.aws_iam_policy_document.cognito_es_policy[0].json
-
 }
 
 
 resource "aws_iam_role" "cognito_es_role" {
   count              = var.cognito_enabled ? 1 : 0
-  name               = "${var.name}-COGNITO-ACCESS-ES-ROLE"
+  name               = "${var.name}_AmazonOpenSearchServiceCognitoAccess"
   assume_role_policy = data.aws_iam_policy_document.es_assume_policy[0].json
-
 }
 
 resource "aws_iam_role_policy_attachment" "cognito_es_attach" {
